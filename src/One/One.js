4b379786354c5cc0e4fc2260e64d4c82e1f189fa
Nikhilkathi <42873004+Nikhilkathi@users.noreply.github.com>
2022-03-24 19:03:33 +0530

    okie

--- src/One/One.js
+++ src/One/One.js
@@ -1,6 +1,19 @@
-function One(){
+import React from 'react'
+const One=()=>{
     return(
-        <div> <h1>Hello World</h1></div>
+        <div className="navbar navbar-dark navbar-extend-lg"> 
+            <section>
+                <ul>
+                    <a className='navbar-brand' href='#'>Pro Stack</a>
+            <li className='navbar-list'><a className='navbar-link' href="#">Home</a></li>
+            <li className=''><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+                </ul>
+            </section>
+            
+            </div>
     )
 }
 export default One
